277b583fa2ba328d3e1bc7c183c64f6228cbaac8
Guilherme Moreira <guilhermemoreira@geekplug.com.br>
2021-05-29 15:39:01 -0300

    first commit

---
+++ panel/pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Develop Panel</h1>
+    </div>
+  )
+}
+
+export default Index
---
+++ site/pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Develop Site</h1>
+    </div>
+  )
+}
+
+export default Index
