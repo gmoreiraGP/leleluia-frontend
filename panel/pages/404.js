0793af72683b016d0667fbe2082d202242d18ec5
Guilherme Moreira <guilhermemoreira@geekplug.com.br>
2021-06-16 18:37:11 -0300

    create 404 error page

---
+++ panel/pages/404.js
@@ -0,0 +1,14 @@
+import Container from '../components/Container'
+import Layout from '../components/Layout'
+
+const Custom404 = () => {
+  return (
+    <Layout>
+      <Container>
+        <h1>Desculpe, não encontramos o que você procura.</h1>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Custom404
